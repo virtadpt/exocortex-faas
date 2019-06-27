34dac988a17c699e71b537e0a3abe47ba167b15a
The Doctor <drwho@virtadpt.net>
2019-06-26 18:11:05 -0700

    Added some more witticisms to the TMR 8-bal.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- 8ball-tmr/handler.py
+++ 8ball-tmr/handler.py
@@ -10,7 +10,12 @@ responses = [
     "You're only doing this because you saw it in a movie, aren't you?",
     "Don't forget the fire extinguisher.",
     "You're gonna want more than eye protection for that one.",
-    "I need the dexterity."
+    "I need the dexterity.",
+    "Why is it that whenever we hang out I find myself in a hazmat suit?",
+    "I grabbed these weeds from a local area.",
+    "Imagine these in the hands of someone who knows what they're doing!",
+    "That looks safe.",
+    "All those years of _Duck Hunt_ finally paid off!"
     ]
 
 def handle(req):
